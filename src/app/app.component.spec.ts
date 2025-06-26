bce849c7a015d17c537e8845de5249ee992a4e27
JoshMith <JoshMith@users.noreply.github.com>
2025-06-26 22:50:08 +0300

    Github Actions2 AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Frontend' title`, () => {
+  it(`should have the 'Hello, Frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Frontend');
