83bbdb1825196eef1ba20d36ca19454ce50e59f1
JoshMith <JoshMith@users.noreply.github.com>
2025-06-27 13:16:20 +0300

    continuing3

--- src/app/shop/shop.component.spec.ts
+++ src/app/shop/shop.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 import { ShopComponent } from './shop.component';
 
 describe('ShopComponent', () => {
@@ -8,9 +9,18 @@ describe('ShopComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ShopComponent]
-    })
-    .compileComponents();
+      imports: [ShopComponent], // Because it's standalone
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}), // mock your route params here if needed
+            snapshot: { paramMap: { get: () => null } }
+          }
+        }
+      ]
+    }).compileComponents();
+
 
     fixture = TestBed.createComponent(ShopComponent);
     component = fixture.componentInstance;
