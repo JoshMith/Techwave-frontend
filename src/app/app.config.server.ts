84642ba4fecfcc26543118a5898f21660ede7aef
JoshMith <JoshMith@users.noreply.github.com>
2025-11-11 04:30:45 +0300

    DV SX

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,13 +1,40 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
+import { provideServerRouting, RenderMode } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRouting(serverRoutes)
+    provideServerRouting([
+      { path: '', renderMode: RenderMode.Server },
+      { path: 'homepage', renderMode: RenderMode.Server },
+      { path: 'shop', renderMode: RenderMode.Server },
+      { path: 'categories/Phones', renderMode: RenderMode.Server },
+      { path: 'categories/Laptops', renderMode: RenderMode.Server },
+      { path: 'categories/Accessories', renderMode: RenderMode.Server },
+      { path: 'categories/Home Appliances', renderMode: RenderMode.Server },
+      { path: 'categories/Gaming', renderMode: RenderMode.Server },
+      { path: 'categories/Audio & Sound', renderMode: RenderMode.Server },
+      { path: 'deals', renderMode: RenderMode.Server },
+      { path: 'cart', renderMode: RenderMode.Server },
+      { path: 'checkout/details', renderMode: RenderMode.Server },
+      { path: 'checkout/payment', renderMode: RenderMode.Server },
+      { path: 'checkout/orders', renderMode: RenderMode.Server },
+      { path: 'profile', renderMode: RenderMode.Server },
+      { path: 'seller-dashboard', renderMode: RenderMode.Server },
+      
+      {
+        path: 'product/:id',
+        renderMode: RenderMode.Client
+      },
+      { path: 'login', renderMode: RenderMode.Server },
+      { path: 'signup', renderMode: RenderMode.Server },
+      { path: 'forgot-pwd', renderMode: RenderMode.Server },
+      { path: 'termsofservice', renderMode: RenderMode.Server },
+      { path: 'privacypolicy', renderMode: RenderMode.Server }
+    ])
   ]
 };
 
