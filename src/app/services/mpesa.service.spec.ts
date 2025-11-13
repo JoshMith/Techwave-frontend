4a273c7540e97215f3620c71c969b8f10874b0e9
JoshMith <JoshMith@users.noreply.github.com>
2025-11-13 14:14:09 +0300

    Mpesa2

--- src/app/services/mpesa.service.spec.ts
+++ src/app/services/mpesa.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MpesaService } from './mpesa.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('MpesaService', () => {
   let service: MpesaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(MpesaService);
   });
 
